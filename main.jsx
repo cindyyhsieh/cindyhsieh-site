b2b936affb329586a976e4e793c42221fd5479fc
cindyyhsieh <cindy06210315@gmail.com>
2025-04-13 20:44:15 -0400

    initial upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => (
+  <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <h1>Cindy Hsieh</h1>
+    <p>Cellist | Performer | Collaborator</p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
